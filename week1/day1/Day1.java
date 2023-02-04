0ad770ef6120fa04327630b3467de1f5389de177
GauthaammanSomasundaram <124450104+GauthaammanSomasundaram@users.noreply.github.com>
2023-02-04 20:48:14 +0530

    Add files via upload
    Week1

---
+++ week1/day1/Day1.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Day1 
+{	
+public static void main(String[] args) 
+{
+	int wheels = 2;
+	short Mirrors = 2;
+	long ChassisNo = 98765432109876543l ;
+	boolean punctured = false;
+	String Bike = "YamahaRayZR";
+	double RunningKm = 1500.65;
+	System.out.println("   Two Wheeler Assisgnment     ");
+	System.out.println("How Many Wheels: "+ wheels);
+	System.out.println("How many Mirrors in the Bike: "+ Mirrors);
+	System.out.println("WHat is the Chassis No: "+ ChassisNo);
+	System.out.println("Is Bike Wheels Punctured: "+ punctured);
+	System.out.println("Brand & name of bike: "+ Bike);
+	System.out.println("How many km driven: "+ RunningKm);
+}
+}
---
+++ week1/day1/Day1Ass2.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class Day1Ass2 
+{
+	public static void main(String[] args) 
+	{
+		String MobileBrand = "Realme" ;
+		char Logo = 'R';
+		short MobilePieces = 20;
+		int ModelNumber = 8;
+		long imeiNumber = 987654321987654L;
+		float MobilePrice = 17999.00f;
+		boolean isDamaged = false;
+		
+		System.out.println("       Mobile Assignment        ");
+		System.out.println("Mobile Brand is: "+ MobileBrand);
+		System.out.println("Logo is: "+ Logo);
+		System.out.println("Mobile Pieces: "+ MobilePieces);
+		System.out.println("Model Number is: "+ ModelNumber);
+		System.out.println("imei Number is: "+ imeiNumber);
+		System.out.println("Mobile Price is: "+ MobilePrice);
+		System.out.println("Is Mobile damaged: "+ isDamaged);
+	}
+}
